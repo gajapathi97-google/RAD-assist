68b82ef73f01bc9ac425d76b02dde88674a2bce5
gajapathi97-google <gajapathi@google.com>
2025-08-22 18:55:07 +0530

    Create assests.ts

---
+++ UI/assests.ts
@@ -0,0 +1,6 @@
+// Centralized asset URLs to improve maintainability.
+
+export const userAvatar = "https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png";
+
+export const preTreatmentScan = "https://storage.mtls.cloud.google.com/rad-assist-uploads-bucket/HCC-TACE-Seg/Pre%20segmented.jpeg";
+export const postTreatmentScan = "https://storage.mtls.cloud.google.com/rad-assist-uploads-bucket/HCC-TACE-Seg/Post%20Segmented.jpeg";
